01b2efa37881240f84b5fe3a5f7553279c9c9bb6
THANHCONG <hienkimthiquang@gmail.com>
2026-04-05 22:58:00 +0700

    bai Thuc hanh ve Fragment

---
+++ BaiTH12_FragmentDong/app/src/main/java/kimhien/baith12_fragmentdong/MainActivity.java
@@ -0,0 +1,27 @@
+package kimhien.baith12_fragmentdong;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.framecontentHolder,new ContentFragment())
+                .add(R.id.frameFooterHolder,new FooterFragment())
+                .commit();
+    }
+}
