5945686c3be35709bf57b52217e9a371115cde49
THANHCONG <hienkimthiquang@gmail.com>
2026-04-07 16:30:41 +0700

    nộp bài Bottom_Navigation_bar

---
+++ Bottom_Navigation_bar/app/src/main/java/kimhien/bottom_navigation_bar/MainActivity.java
@@ -0,0 +1,49 @@
+package kimhien.bottom_navigation_bar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navigation = findViewById(R.id.navigation);
+
+        // load mặc định
+        loadFragment(new HomeFragment());
+
+        navigation.setOnItemSelectedListener(item -> {
+
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                loadFragment(new HomeFragment());
+                return true;
+            } else if (id == R.id.nav_cart) {
+                loadFragment(new CartFragment());
+                return true;
+            } else if (id == R.id.nav_profile) {
+                loadFragment(new ProfileFragment());
+                return true;
+            }
+
+            return false;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_container, fragment)
+                .commit();
+    }
+}
---
+++ Bottom_Navigation_bar/app/src/main/java/kimhien/bottom_navigation_bar/ProfileFragment.java
@@ -0,0 +1,17 @@
+package kimhien.bottom_navigation_bar;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ProfileFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
